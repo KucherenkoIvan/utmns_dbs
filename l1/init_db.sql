5a54f7199ef7cecd37921c0bd758ace34ccd457b
Ivan Kucherenko <stysha.722@gmail.com>
2023-02-18 10:00:21 +0500

    init: 2

--- l1/init_db.sql
+++ l1/init_db.sql
@@ -4,7 +4,7 @@ create table if not exists regions (
 );
 
 create table if not exists countries (
-	country_id int not null primary key,
+	country_id varchar(3) not null primary key,
 	country_name varchar(50) not null,
 	region_id int not null references regions(region_id)
 );
@@ -15,15 +15,35 @@ create table if not exists locations (
 	postal_code varchar(20) not null,
 	city varchar(50) not null,
 	state_province varchar(100),
-	country_id int not null references countries(country_id)
+	country_id varchar(3) null references countries(country_id)
 );
 
 create table if not exists jobs (
-	job_id int not null primary key,
+	job_id not null varchar(15) null primary key,
 	job_title varchar(100) not null,
 	min_salary int not null,
 	max_salary int not null
 );
 
 create table if not exists departments (
+	department_id int not null primary key,
+	department_name varchar(50) not null,
+	manager_id int not null,
+	location_id int not null references locations(location_id)
+);
+
+create table employees (
+	employee id int not null primary key,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	email varchar(100) not null,
+	phone_number varchar(15) not null,
+	hire_date date not null,
+	job_id varchar(15) not null references jobs(job_id),
+	salary int not null,
+	commission_pct int,
+	manager_id int references employee(employee_id),
+	department_id int not null references departments(department_id)
 )
+
+
